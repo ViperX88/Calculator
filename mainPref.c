3225b905e7c6f927fb851f63389843b86a537284
Tareq <124922718+ViperX88@users.noreply.github.com>
2023-03-20 19:59:50 +0100

    Update mainPref.c

--- mainPref.c
+++ mainPref.c
@@ -1,6 +1,5 @@
 /*
  File: mainPref.c
- Authors: Kristaps Melbardis (k.t.melbardis@student.rug.nl), Tareq Ahrari (t.ahrari@student.rug.nl)
  Description: Program that outputs expressions with brackets, and if the expression is not numerical, also its value
 */
 
